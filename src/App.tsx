321bd8d5c6cd1b27844d2694a041b06bd6fcb818
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-10-15 10:03:38 +0200

    ops: init react app with vite-pwa

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import PWABadge from './PWABadge.tsx'
+
+function App() {
+  return (
+    <>
+      <h1>Equilibre</h1>
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Equilibre',
+      short_name: 'equilibre',
+      description: 'Une application pour le suivi des humeur et la gestion facilitée de sa santé mentale',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
